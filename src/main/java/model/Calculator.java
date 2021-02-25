9929c1c10b0eb8474351fc1eebbc886a56265bf6
Evgenii Buivolov <zhekadreamteam11@gmail.com>
2021-02-25 23:28:39 +0300

    Update: create failing test

---
+++ src/main/java/model/Calculator.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Calculator {
+    private double result;
+
+    public Calculator() {
+        this.result = 0.0;
+    }
+
+    public double plus(double number) {
+        return 0.0;
+    }
+}
---
+++ src/test/java/model/CalculatorTest.java
@@ -0,0 +1,15 @@
+package model;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void plus() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.plus(2));
+    }
+}
