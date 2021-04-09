1dbf2c002b140565b5330988e87ffdb2bb9507d1
ereborDeveloper <zhekadreamteam11@gmail.com>
2021-04-09 05:04:19 +0300

    Update: Vue.js config

---
+++ src/main/resources/static/js/pages/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>Hekki</div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: 'Hell Vue!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
